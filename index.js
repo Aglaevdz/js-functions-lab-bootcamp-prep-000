fdee818a20e732eb0838b836b756f1b9613dfed8
Aglae Verduzco <aglae.cv@gmail.com>
2018-09-19 01:30:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+
+function happyHolidays() {
+return "Happy holidays!"
+}
+
+var greeting = "Happy holidays"
+var Name = "you"
+function happyHolidaysTo(Name) {
+  return `${"Happy holidays"}, ${Name}!`
+}
+
+function happyHolidaysTo () {
+  if ("holiday === Independence Day") ;
+return `Happy ${holiday}, ${name}!`
+}
