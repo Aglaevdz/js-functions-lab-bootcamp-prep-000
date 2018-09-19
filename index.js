971962d6cb9b5dd926c1d8c53bbe77d0276d17cf
Aglae Verduzco <aglae.cv@gmail.com>
2018-09-19 01:37:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ return "Happy holidays!"
 var greeting = "Happy holidays"
 var Name = "Happy holidays, you!"
 function happyHolidaysTo(Name) {
-  return `${"greeting"}, ${Name}!`
+  return `${Name}!`
 }
 
 function happyHolidaysTo () {
